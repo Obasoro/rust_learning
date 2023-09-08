b005257e8dec25bfc564a9d78159c1309c8bdd9d
obasoro <obasorokunle@gmail.com>
2023-09-08 09:07:10 -0500

    Learning_Rust from scratch

---
+++ README.md
@@ -0,0 +1,14 @@
+`cargo new hello_cargo` installing cargo
+
+`cargo build`
+
+`cargo run`
+
+`cargo check`
+
+`cargo build`
+
+`cargo run`
+
+`cargo update`
+
---
+++ cargo/rand_number/src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Guess the imput.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error reading");
+
+    println!("Guess the number {guess}");
+
+}
